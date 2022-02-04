b9591c511be23a4c5cbbb513c17f43927f0d9cd5
KlaraB84 <97805470+KlaraB84@users.noreply.github.com>
2022-02-04 14:29:45 +0100

    Initial commit

---
+++ src/com/company/Actor.java
@@ -0,0 +1,28 @@
+package com.company;
+public class Actor extends Staff {
+
+    SexEnum sex;
+
+    public Actor(String name, String lastName, double salary, SexEnum sex) {
+        super(name, lastName, salary);
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "sex=" + sex +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public SexEnum getSex() {
+        return sex;
+    }
+
+    public void setSex(SexEnum sex) {
+        this.sex = sex;
+    }
+}
---
+++ src/com/company/MovieTypeEnum.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public enum MovieTypeEnum {
+    horror,
+    action,
+    comedy,
+    romantic
+}
+
